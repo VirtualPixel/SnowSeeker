972ea87fdab64f7a0a194f948926166e9f7951d1
Justin Wells <wellsmjustin@gmail.com>
2022-12-13 06:11:38 -0500

    Initial Commit

---
+++ SnowSeeker/SnowSeekerApp.swift
@@ -0,0 +1,17 @@
+//
+//  SnowSeekerApp.swift
+//  SnowSeeker
+//
+//  Created by Justin Wells on 12/13/22.
+//
+
+import SwiftUI
+
+@main
+struct SnowSeekerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
